b881ea1c12b73465388cfecd2ff83692ad64abfc
Andre <sarreqiandrea@gmail.com>
2022-12-19 21:58:34 +0000

    Add code to increment score and incorrect answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,56 +44,76 @@ function checkAnswer() {
     let userAnswer = parseInt(document.getElementById("answer-box").value);
     let calculatedAnswer = calculateCorrectAnswer();
     let isCorrect = userAnswer === calculatedAnswer[0];
-
-    if (isCorrect) {
-        alert("Hey! You got it right! :D");
-    } else {
-        alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
-    }
-
-    runGame(calculatedAnswer[1]);
-
 }
+    function checkAnswer() {
 
-/**
- * Gets the operands (the numbers) and the operator (plus, minus etc)
- * directly from the dom, and returns the correct answer.
- */
-function calculateCorrectAnswer() {
-
-    let operand1 = parseInt(document.getElementById('operand1').innerText);
-    let operand2 = parseInt(document.getElementById('operand2').innerText);
-    let operator = document.getElementById("operator").innerText;
-
-    if (operator === "+") {
-        return [operand1 + operand2, "addition"];
-    } else {
-        alert(`Unimplemented operator ${operator}`);
-        throw `Unimplemented operator ${operator}. Aborting!`;
-    }
-
-}
-
-function incrementScore() {
-
-}
-
-function incrementWrongAnswer() {
-
-}
-
-function displayAdditionQuestion(operand1, operand2) {
-
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').textContent = "+";
+        let userAnswer = parseInt(document.getElementById("answer-box").value);
+        let calculatedAnswer = calculateCorrectAnswer();
+        let isCorrect = userAnswer === calculatedAnswer[0];
     
-}
-
-function displaySubtractQuestion() {
-
-}
-
-function displayMultiplyQuestion() {
+        if (isCorrect) {
+            alert("Hey! You got it right! :D");
+            incrementScore();
+        } else {
+            alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+            incrementWrongAnswer();
+        }
     
-}
+        runGame(calculatedAnswer[1]);
+    
+    }
+    
+    /**
+     * Gets the operands (the numbers) and the operator (plus, minus etc)
+     * directly from the dom, and returns the correct answer.
+     */
+    function calculateCorrectAnswer() {
+    
+        let operand1 = parseInt(document.getElementById('operand1').innerText);
+        let operand2 = parseInt(document.getElementById('operand2').innerText);
+        let operator = document.getElementById("operator").innerText;
+    
+        if (operator === "+") {
+            return [operand1 + operand2, "addition"];
+        } else {
+            alert(`Unimplemented operator ${operator}`);
+            throw `Unimplemented operator ${operator}. Aborting!`;
+        }
+    
+    }
+    
+    /**
+     * Gets the current score from the DOM and increments it by 1
+     */
+    function incrementScore() {
+    
+        let oldScore = parseInt(document.getElementById("score").innerText);
+        document.getElementById("score").innerText = ++oldScore;
+    
+    }
+    
+    /**
+     * Gets the current tally of incorrect answers from the DOM and increments it by 1
+     */
+    function incrementWrongAnswer() {
+    
+        let oldScore = parseInt(document.getElementById("incorrect").innerText);
+        document.getElementById("incorrect").innerText = ++oldScore;
+        
+    }
+    
+    function displayAdditionQuestion(operand1, operand2) {
+    
+        document.getElementById('operand1').textContent = operand1;
+        document.getElementById('operand2').textContent = operand2;
+        document.getElementById('operator').textContent = "+";
+        
+    }
+    
+    function displaySubtractQuestion() {
+    
+    }
+    
+    function displayMultiplyQuestion() {
+        
+    }
